a5a38e13229ce525ea1611c2d6b89ca41d4400af
estik567 <117013498+estik567@users.noreply.github.com>
2023-05-15 19:41:19 +0300

    Update Program.cs

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -21,12 +21,12 @@ app.MapGet("/items", async (ToDoDbContext context) => { return await context.Ite
 
 app.MapGet("/",()=>"ToDoAPI is running");
 
-app.MapPost("/items", async (ToDoDbContext context, Item item) =>
+app.MapPost("/items", async (ToDoDbContext context, Items item) =>
 {
-    EntityEntry<Item> itemToReturn = context.Items.Add(item); await context.SaveChangesAsync();
+    EntityEntry<Items> itemToReturn = context.Items.Add(item); await context.SaveChangesAsync();
     return itemToReturn.Entity;
 });
-app.MapPut("/items", async (ToDoDbContext context, Item item) =>
+app.MapPut("/items", async (ToDoDbContext context, Items item) =>
 {
     var entity = context.Items.Update(item);
     await context.SaveChangesAsync();
